9d99466c2b0613949f4b9d5a2d7c6c1bcb0430d3
joe-nghiem-goldenowl <joe.nghiem.goldenowl@gmail.com>
2023-05-11 22:36:40 +0700

    Add linter, swagger Add ENV Use Postgres

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -11,20 +11,20 @@ class RegisterSerializer(serializers.ModelSerializer):
     )
 
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-    password2 = serializers.CharField(write_only=True, required=True)
+    password_confirmation = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = User
-        fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name')
+        fields = ('username', 'password', 'password_confirmation', 'email', 'first_name', 'last_name')
 
     def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
+        if attrs['password'] != attrs['password_confirmation']:
             raise serializers.ValidationError({"password": "Password fields didn't match."})
 
         return attrs
 
     def create(self, validated_data):
-        del validated_data['password2']
+        del validated_data['password_confirmation']
         user = User.objects.create(**validated_data)
 
         user.set_password(validated_data['password'])
