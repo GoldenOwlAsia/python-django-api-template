d8053c899ef7de1edf018283d779bcf9e0658626
joe-nghiem-goldenowl <joe.nghiem.goldenowl@gmail.com>
2023-12-29 16:35:08 +0700

    Write unit test for register view

--- authentication/models/customer.py
+++ authentication/models/customer.py
@@ -6,7 +6,7 @@ from django.db import models
 class Customer(User):
     bio = models.TextField(verbose_name="Bio info", max_length=500, blank=True)
     birth_date = models.DateField(verbose_name="Birth date", null=True, blank=True)
-    address = models.CharField(verbose_name="Address", max_length=30, blank=True)
+    address = models.CharField(verbose_name="Address", max_length=200, blank=True)
 
     class Meta:
         db_table = "customer"
--- authentication/tests/test_views.py
+++ authentication/tests/test_views.py
@@ -1,22 +1,30 @@
 from django.contrib.auth.models import User
+from authentication.factories.customer_factory import CustomerFactory
 from django.test import TestCase
 from rest_framework_simplejwt.tokens import RefreshToken
+from authentication.models import Customer
+from faker import Faker
 
+fake = Faker()
 
 class TestCalls(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.user = User.objects.create_user(
-            username="nguyen.hung",
-            password="1234ABbcd!@",
-            email="nguyen.hung@gmail.com",
+        cls.password = fake.password()
+        cls.username = fake.profile()["username"]
+        cls.email = fake.email()
+        cls.customer = User.objects.create_user(
+            username=cls.username,
+            password=cls.password,
+            email=cls.email,
         )
 
     def test_call_register(self):
+        user_name = fake.profile()["username"]
         data = {
-            "username": "bao.binh",
-            "password": "1234ABbcd!@",
-            "email": "bao.binh@gmail.com",
+            "username": user_name,
+            "password": fake.password(),
+            "email": fake.email(),
         }
 
         response = self.client.post(
@@ -26,21 +34,20 @@ class TestCalls(TestCase):
         self.assertEqual(response.status_code, 201)
         self.assertIn("username", response.json())
         self.assertIn("email", response.json())
-        self.assertEqual(response.json().get("username"), "bao.binh")
+        self.assertEqual(response.json().get("username"), user_name)
 
     def test_call_login(self):
-        data = {"username": "nguyen.hung", "password": "1234ABbcd!@"}
+        data = {"username": TestCalls.username, "password": TestCalls.password}
 
         response = self.client.post(
             "/api/v1/auth/login/", data, content_type="application/json"
         )
-
         self.assertEqual(response.status_code, 200)
         self.assertIn("access", response.json())
         self.assertIn("refresh", response.json())
 
     def test_call_refresh_token(self):
-        refresh = RefreshToken.for_user(TestCalls.user)
+        refresh = RefreshToken.for_user(TestCalls.customer)
         data = {
             "refresh": str(refresh),
         }
